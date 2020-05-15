b7bc28792a754e411f2a47e3f229fe4d84c378a8
Kevin Chen <kevinmichaelchen@gmail.com>
2020-05-15 18:34:29 -0400

    Auto Creation of Proto

--- alpacalabs/confirmation/v1/api_create.pb.go
+++ alpacalabs/confirmation/v1/api_create.pb.go
@@ -20,78 +20,82 @@ var _ = math.Inf
 // proto package needs to be updated.
 const _ = proto.ProtoPackageIsVersion3 // please upgrade the proto package
 
-type CreateEmailAddressConfirmationCode struct {
+type CreateEmailAddressConfirmationCodeRequest struct {
 	EmailAddressId       string   `protobuf:"bytes,1,opt,name=email_address_id,json=emailAddressId,proto3" json:"email_address_id,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
 }
 
-func (m *CreateEmailAddressConfirmationCode) Reset()         { *m = CreateEmailAddressConfirmationCode{} }
-func (m *CreateEmailAddressConfirmationCode) String() string { return proto.CompactTextString(m) }
-func (*CreateEmailAddressConfirmationCode) ProtoMessage()    {}
-func (*CreateEmailAddressConfirmationCode) Descriptor() ([]byte, []int) {
+func (m *CreateEmailAddressConfirmationCodeRequest) Reset() {
+	*m = CreateEmailAddressConfirmationCodeRequest{}
+}
+func (m *CreateEmailAddressConfirmationCodeRequest) String() string { return proto.CompactTextString(m) }
+func (*CreateEmailAddressConfirmationCodeRequest) ProtoMessage()    {}
+func (*CreateEmailAddressConfirmationCodeRequest) Descriptor() ([]byte, []int) {
 	return fileDescriptor_d83313b0b285105e, []int{0}
 }
 
-func (m *CreateEmailAddressConfirmationCode) XXX_Unmarshal(b []byte) error {
-	return xxx_messageInfo_CreateEmailAddressConfirmationCode.Unmarshal(m, b)
+func (m *CreateEmailAddressConfirmationCodeRequest) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_CreateEmailAddressConfirmationCodeRequest.Unmarshal(m, b)
 }
-func (m *CreateEmailAddressConfirmationCode) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	return xxx_messageInfo_CreateEmailAddressConfirmationCode.Marshal(b, m, deterministic)
+func (m *CreateEmailAddressConfirmationCodeRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_CreateEmailAddressConfirmationCodeRequest.Marshal(b, m, deterministic)
 }
-func (m *CreateEmailAddressConfirmationCode) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_CreateEmailAddressConfirmationCode.Merge(m, src)
+func (m *CreateEmailAddressConfirmationCodeRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_CreateEmailAddressConfirmationCodeRequest.Merge(m, src)
 }
-func (m *CreateEmailAddressConfirmationCode) XXX_Size() int {
-	return xxx_messageInfo_CreateEmailAddressConfirmationCode.Size(m)
+func (m *CreateEmailAddressConfirmationCodeRequest) XXX_Size() int {
+	return xxx_messageInfo_CreateEmailAddressConfirmationCodeRequest.Size(m)
 }
-func (m *CreateEmailAddressConfirmationCode) XXX_DiscardUnknown() {
-	xxx_messageInfo_CreateEmailAddressConfirmationCode.DiscardUnknown(m)
+func (m *CreateEmailAddressConfirmationCodeRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_CreateEmailAddressConfirmationCodeRequest.DiscardUnknown(m)
 }
 
-var xxx_messageInfo_CreateEmailAddressConfirmationCode proto.InternalMessageInfo
+var xxx_messageInfo_CreateEmailAddressConfirmationCodeRequest proto.InternalMessageInfo
 
-func (m *CreateEmailAddressConfirmationCode) GetEmailAddressId() string {
+func (m *CreateEmailAddressConfirmationCodeRequest) GetEmailAddressId() string {
 	if m != nil {
 		return m.EmailAddressId
 	}
 	return ""
 }
 
-type CreatePhoneNumberConfirmationCode struct {
+type CreatePhoneNumberConfirmationCodeRequest struct {
 	PhoneNumberId        string   `protobuf:"bytes,1,opt,name=phone_number_id,json=phoneNumberId,proto3" json:"phone_number_id,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
 }
 
-func (m *CreatePhoneNumberConfirmationCode) Reset()         { *m = CreatePhoneNumberConfirmationCode{} }
-func (m *CreatePhoneNumberConfirmationCode) String() string { return proto.CompactTextString(m) }
-func (*CreatePhoneNumberConfirmationCode) ProtoMessage()    {}
-func (*CreatePhoneNumberConfirmationCode) Descriptor() ([]byte, []int) {
+func (m *CreatePhoneNumberConfirmationCodeRequest) Reset() {
+	*m = CreatePhoneNumberConfirmationCodeRequest{}
+}
+func (m *CreatePhoneNumberConfirmationCodeRequest) String() string { return proto.CompactTextString(m) }
+func (*CreatePhoneNumberConfirmationCodeRequest) ProtoMessage()    {}
+func (*CreatePhoneNumberConfirmationCodeRequest) Descriptor() ([]byte, []int) {
 	return fileDescriptor_d83313b0b285105e, []int{1}
 }
 
-func (m *CreatePhoneNumberConfirmationCode) XXX_Unmarshal(b []byte) error {
-	return xxx_messageInfo_CreatePhoneNumberConfirmationCode.Unmarshal(m, b)
+func (m *CreatePhoneNumberConfirmationCodeRequest) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_CreatePhoneNumberConfirmationCodeRequest.Unmarshal(m, b)
 }
-func (m *CreatePhoneNumberConfirmationCode) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	return xxx_messageInfo_CreatePhoneNumberConfirmationCode.Marshal(b, m, deterministic)
+func (m *CreatePhoneNumberConfirmationCodeRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_CreatePhoneNumberConfirmationCodeRequest.Marshal(b, m, deterministic)
 }
-func (m *CreatePhoneNumberConfirmationCode) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_CreatePhoneNumberConfirmationCode.Merge(m, src)
+func (m *CreatePhoneNumberConfirmationCodeRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_CreatePhoneNumberConfirmationCodeRequest.Merge(m, src)
 }
-func (m *CreatePhoneNumberConfirmationCode) XXX_Size() int {
-	return xxx_messageInfo_CreatePhoneNumberConfirmationCode.Size(m)
+func (m *CreatePhoneNumberConfirmationCodeRequest) XXX_Size() int {
+	return xxx_messageInfo_CreatePhoneNumberConfirmationCodeRequest.Size(m)
 }
-func (m *CreatePhoneNumberConfirmationCode) XXX_DiscardUnknown() {
-	xxx_messageInfo_CreatePhoneNumberConfirmationCode.DiscardUnknown(m)
+func (m *CreatePhoneNumberConfirmationCodeRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_CreatePhoneNumberConfirmationCodeRequest.DiscardUnknown(m)
 }
 
-var xxx_messageInfo_CreatePhoneNumberConfirmationCode proto.InternalMessageInfo
+var xxx_messageInfo_CreatePhoneNumberConfirmationCodeRequest proto.InternalMessageInfo
 
-func (m *CreatePhoneNumberConfirmationCode) GetPhoneNumberId() string {
+func (m *CreatePhoneNumberConfirmationCodeRequest) GetPhoneNumberId() string {
 	if m != nil {
 		return m.PhoneNumberId
 	}
@@ -99,8 +103,8 @@ func (m *CreatePhoneNumberConfirmationCode) GetPhoneNumberId() string {
 }
 
 func init() {
-	proto.RegisterType((*CreateEmailAddressConfirmationCode)(nil), "alpacalabs.confirmation.v1.CreateEmailAddressConfirmationCode")
-	proto.RegisterType((*CreatePhoneNumberConfirmationCode)(nil), "alpacalabs.confirmation.v1.CreatePhoneNumberConfirmationCode")
+	proto.RegisterType((*CreateEmailAddressConfirmationCodeRequest)(nil), "alpacalabs.confirmation.v1.CreateEmailAddressConfirmationCodeRequest")
+	proto.RegisterType((*CreatePhoneNumberConfirmationCodeRequest)(nil), "alpacalabs.confirmation.v1.CreatePhoneNumberConfirmationCodeRequest")
 }
 
 func init() {
@@ -108,22 +112,22 @@ func init() {
 }
 
 var fileDescriptor_d83313b0b285105e = []byte{
-	// 258 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0xd2, 0x4e, 0xcc, 0x29, 0x48,
-	0x4c, 0x4e, 0xcc, 0x49, 0x4c, 0x2a, 0xd6, 0x4f, 0xce, 0xcf, 0x4b, 0xcb, 0x2c, 0xca, 0x4d, 0x2c,
-	0xc9, 0xcc, 0xcf, 0xd3, 0x2f, 0x33, 0xd4, 0x4f, 0x2c, 0xc8, 0x8c, 0x4f, 0x2e, 0x4a, 0x4d, 0x2c,
-	0x49, 0xd5, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x92, 0x42, 0x28, 0xd6, 0x43, 0x56, 0xac, 0x57,
-	0x66, 0xa8, 0xe4, 0xc7, 0xa5, 0xe4, 0x0c, 0x56, 0xeb, 0x9a, 0x9b, 0x98, 0x99, 0xe3, 0x98, 0x92,
-	0x52, 0x94, 0x5a, 0x5c, 0xec, 0x8c, 0xa4, 0xc8, 0x39, 0x3f, 0x25, 0x55, 0x48, 0x83, 0x4b, 0x20,
-	0x15, 0x24, 0x1f, 0x9f, 0x08, 0x51, 0x10, 0x9f, 0x99, 0x22, 0xc1, 0xa8, 0xc0, 0xa8, 0xc1, 0x19,
-	0xc4, 0x97, 0x8a, 0xa4, 0xcf, 0x33, 0x45, 0xc9, 0x9b, 0x4b, 0x11, 0x62, 0x5e, 0x40, 0x46, 0x7e,
-	0x5e, 0xaa, 0x5f, 0x69, 0x6e, 0x52, 0x6a, 0x11, 0x86, 0x71, 0x6a, 0x5c, 0xfc, 0x05, 0x20, 0xe9,
-	0xf8, 0x3c, 0xb0, 0x3c, 0xc2, 0x34, 0xde, 0x02, 0x84, 0x2e, 0xcf, 0x14, 0xa7, 0x83, 0x8c, 0x5c,
-	0x72, 0xc9, 0xf9, 0xb9, 0x7a, 0xb8, 0xdd, 0xef, 0x24, 0x88, 0x6c, 0x78, 0x00, 0xc8, 0xbb, 0x01,
-	0x8c, 0x51, 0x5e, 0xe9, 0x99, 0x25, 0x19, 0xa5, 0x49, 0x7a, 0xc9, 0xf9, 0xb9, 0xfa, 0x8e, 0x60,
-	0xbd, 0x3e, 0xa0, 0x80, 0x02, 0x87, 0x46, 0x51, 0x6a, 0x41, 0xbe, 0x2e, 0xb2, 0x29, 0xba, 0xe9,
-	0xf9, 0xfa, 0xb8, 0x43, 0x73, 0x11, 0x13, 0xb3, 0xa3, 0x8f, 0xf3, 0x2a, 0x26, 0x29, 0x84, 0x39,
-	0x7a, 0xc8, 0x56, 0xea, 0x85, 0x19, 0x9e, 0x42, 0x96, 0x8c, 0x41, 0x96, 0x8c, 0x09, 0x33, 0x4c,
-	0x62, 0x03, 0xdb, 0x6a, 0x0c, 0x08, 0x00, 0x00, 0xff, 0xff, 0x6b, 0x43, 0x0a, 0x31, 0xb2, 0x01,
-	0x00, 0x00,
+	// 263 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x74, 0x90, 0xbd, 0x6a, 0xc3, 0x30,
+	0x14, 0x85, 0x71, 0x0a, 0x85, 0x0a, 0xfa, 0x97, 0xa9, 0x64, 0x28, 0x25, 0x43, 0x71, 0x29, 0x91,
+	0x30, 0x7d, 0x02, 0xc7, 0x74, 0x48, 0x09, 0xc5, 0x18, 0x9a, 0xa1, 0x04, 0x8c, 0x2c, 0xdd, 0x26,
+	0x02, 0xcb, 0x57, 0x95, 0xe5, 0x3c, 0x50, 0xc7, 0xbe, 0x45, 0xd7, 0x3e, 0x55, 0xb0, 0x32, 0x58,
+	0x8b, 0xe7, 0xfb, 0x9d, 0xef, 0x70, 0x0f, 0x79, 0xe6, 0xb5, 0xe1, 0x82, 0xd7, 0xbc, 0x6a, 0x99,
+	0xc0, 0xe6, 0x4b, 0x59, 0xcd, 0x9d, 0xc2, 0x86, 0x1d, 0x12, 0xc6, 0x8d, 0x2a, 0x85, 0x05, 0xee,
+	0x80, 0x1a, 0x8b, 0x0e, 0xa7, 0xb3, 0x01, 0xa6, 0x21, 0x4c, 0x0f, 0xc9, 0xfc, 0x83, 0x3c, 0x65,
+	0x9e, 0x7d, 0xd5, 0x5c, 0xd5, 0xa9, 0x94, 0x16, 0xda, 0x36, 0x0b, 0xa0, 0x0c, 0x25, 0x14, 0xf0,
+	0xdd, 0x41, 0xeb, 0xa6, 0x31, 0xb9, 0x81, 0x1e, 0x2b, 0xf9, 0x89, 0x2b, 0x95, 0xbc, 0x8b, 0x1e,
+	0xa2, 0xf8, 0xa2, 0xb8, 0x82, 0x20, 0xbe, 0x92, 0xf3, 0x82, 0xc4, 0x27, 0x6d, 0xbe, 0xc7, 0x06,
+	0xde, 0x3b, 0x5d, 0x81, 0x1d, 0xb3, 0x3e, 0x92, 0x6b, 0xd3, 0x53, 0x65, 0xe3, 0xb1, 0x41, 0x7a,
+	0x69, 0x86, 0xf0, 0x4a, 0x2e, 0xff, 0x22, 0x72, 0x2f, 0x50, 0xd3, 0xf1, 0x6f, 0x96, 0xb7, 0x61,
+	0x47, 0xde, 0x3f, 0x9f, 0x47, 0x9f, 0x6f, 0x3b, 0xe5, 0xf6, 0x5d, 0x45, 0x05, 0x6a, 0x96, 0xfa,
+	0xec, 0xba, 0x9f, 0xcd, 0x6f, 0x63, 0xc1, 0xe0, 0x22, 0xb4, 0x2c, 0x76, 0xc8, 0xc6, 0xb7, 0xfd,
+	0x99, 0x9c, 0xa5, 0xeb, 0xec, 0x77, 0x32, 0x1b, 0x3c, 0x34, 0xac, 0xa4, 0x9b, 0xe4, 0x3f, 0x3c,
+	0x6e, 0xc3, 0xe3, 0x76, 0x93, 0x54, 0xe7, 0xbe, 0xf5, 0xe5, 0x18, 0x00, 0x00, 0xff, 0xff, 0x77,
+	0x98, 0xb9, 0xe4, 0xc0, 0x01, 0x00, 0x00,
 }
